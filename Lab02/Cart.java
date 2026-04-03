1cea11b539c2063a4e2fa251b49e4ffd4284addd
dirus182 <146466664+dirus182@users.noreply.github.com>
2026-04-03 22:04:03 +0700

    update

---
+++ Lab02/Cart.java
@@ -0,0 +1,43 @@
+package AimsProject;
+
+public class Cart {
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	private int qtyOrdered = 0;
+	private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+	public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+		if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+			itemsOrdered[qtyOrdered] = disc;
+			qtyOrdered++;
+		} else {
+			System.out.println("Full storage");
+		}
+	}
+
+	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+		boolean found = false;
+		for (int i = 0; i < qtyOrdered; i++) {
+			if (itemsOrdered[i] == disc) {
+				for (int j = i; j < qtyOrdered - 1; j++) {
+					itemsOrdered[j] = itemsOrdered[j + 1];
+				}
+				itemsOrdered[qtyOrdered - 1] = null;
+				qtyOrdered--;
+				found = true;
+				System.out.println("Has already removed");
+				break;
+			}
+		}
+		if (!found) {
+			System.out.println("No found");
+		}
+	}
+
+	public float totalCost() {
+		float total = 0.0f;
+		for (int k = 0; k < qtyOrdered; k++) {
+			total += itemsOrdered[k].getCost();
+		}
+		return total;
+	}
+}
