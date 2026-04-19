5629e5e77378d4729f1c90eec334c7b9240657ee
buituandat1822004@gmail <146466664+dirus182@users.noreply.github.com>
2026-04-20 01:27:58 +0700

    update CartTest

--- AimsProject/src/AimsProject/Cart.java
+++ AimsProject/src/AimsProject/Cart.java
@@ -14,6 +14,44 @@ public class Cart {
 		}
 	}
 
+	public void print() {
+		System.out.println("***********************CART***********************");
+		System.out.println("Ordered Items:");
+		// Duyệt qua mảng itemsOrdered (giả sử qtyOrdered là số lượng hiện tại)
+		for (int i = 0; i < qtyOrdered; i++) {
+			DigitalVideoDisc dvd = itemsOrdered[i];
+			System.out.println((i + 1) + ".DVD - " + dvd.getTitle() + " - " + dvd.getCategory() + " - "
+					+ dvd.getDirector() + " - " + dvd.getLength() + ": " + dvd.getCost() + " $");
+		}
+		System.out.println("Total cost: " + totalCost() + " $");
+		System.out.println("***************************************************");
+	}
+
+	public void searchById(int id) {
+		boolean found = false;
+		for (int i = 0; i < qtyOrdered; i++) {
+			if (itemsOrdered[i].getId() == id) {
+				System.out.print("Found : " + itemsOrdered[i].toString());
+				found = true;
+				break;
+			}
+		}
+	}
+
+	public void searchByTitle(String title) {
+		boolean found = false;
+		for (int i = 0; i < qtyOrdered; i++) {
+			// So sánh title (bỏ qua hoa thường)
+			if (itemsOrdered[i].getTitle().toLowerCase().contains(title.toLowerCase())) {
+				System.out.println("Found : " + itemsOrdered[i].toString());
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("No DVD found with title: " + title);
+		}
+	}
+
 	public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
 		boolean found = false;
 		for (int i = 0; i < qtyOrdered; i++) {
