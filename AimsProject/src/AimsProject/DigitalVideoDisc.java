581a39db0a26a4adba4d3ed199379544664d1ca3
buituandat1822004@gmail <146466664+dirus182@users.noreply.github.com>
2026-04-19 23:30:29 +0700

    git update

--- AimsProject/src/AimsProject/DigitalVideoDisc.java
+++ AimsProject/src/AimsProject/DigitalVideoDisc.java
@@ -6,6 +6,11 @@ public class DigitalVideoDisc {
 	private String director;
 	private int length;
 	private float cost;
+	// --- PHẦN MỚI THÊM ---
+	// Thành viên lớp (Class Member): Cả lớp dùng chung 1 biến này
+	private static int nbDigitalVideoDiscs = 0;
+	// Thành viên thể hiện (Instance Member): Mỗi đối tượng có 1 ID riêng
+	private int id;
 
 	public DigitalVideoDisc() {
 
@@ -13,12 +18,18 @@ public class DigitalVideoDisc {
 
 	public DigitalVideoDisc(String title) {
 		this.title = title;
+		// Tăng biến tổng của lớp lên 1
+		nbDigitalVideoDiscs++;
+		// Gán giá trị hiện tại của biến tổng cho id của đối tượng này
+		this.id = nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String category, String title, float cost) {
 		this.category = category;
 		this.title = title;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String director, String category, String title, float cost) {
@@ -26,6 +37,8 @@ public class DigitalVideoDisc {
 		this.category = category;
 		this.title = title;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
@@ -34,6 +47,8 @@ public class DigitalVideoDisc {
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		nbDigitalVideoDiscs++;
+		this.id = nbDigitalVideoDiscs;
 	}
 
 	public String getTitle() {
@@ -54,6 +69,10 @@ public class DigitalVideoDisc {
 
 	public float getCost() {
 		return cost;
+	}
+
+	public int getId() {
+		return id;
 	}
 
 }
